7d5401ffc7fcf065691f321df7261c67f9cfab72
Eduard Novikov <edyard.novikov.d@gmail.com>
2023-08-31 20:18:03 +0700

    feat: реализовать первую страницу с досками и добавлением их

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,19 @@
+'use client'
 
-export default function Home() {
+import Adder from "@/components/Adder/Adder";
+import Board from "@/components/Board/Board";
+import { useBoards } from "@/hooks/useBoards";
+import useBoardsActions from "@/hooks/useBoardsActions";
+
+
+export default function HomePage() {
+  const {createBoard} = useBoardsActions()
+  
+  const boards = useBoards()
   return (
-    <main>
-      
+    <main className="p-10 flex flex-wrap h-screen content-start">
+      {boards.boards.map(board => <Board title={board.title} id={board.id} key={board.id}/>)}
+      <Adder title='Создать новую доску' create={createBoard}/>
     </main>
   )
 }
